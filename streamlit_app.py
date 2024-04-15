6b14d2ddee04710e5069ab96119dd5b7b1637cc0
AnniaLevko <57740640+AnniaLevko@users.noreply.github.com>
2024-04-15 11:04:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
     """)
 
 
-name_on_order = st.text_input('Name on Smoothie:')
+name_on_order = st.text_input('Your Name','Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
