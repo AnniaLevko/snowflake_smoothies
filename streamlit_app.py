ab3ac0373469366fb9338c3167bc7feae4390940
AnniaLevko <57740640+AnniaLevko@users.noreply.github.com>
2024-04-15 10:00:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 
 ##session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
 
@@ -28,7 +29,7 @@ ingredient_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max
 if ingredient_list:
     ingredient_string = ''
     for fruit_chosen in ingredient_list:
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         ##API call
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
