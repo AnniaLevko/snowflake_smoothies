55a76ee795d7a05e8c27da573d49277a96494003
AnniaLevko <57740640+AnniaLevko@users.noreply.github.com>
2024-04-15 10:21:39 -0400

    Update streamlit_app.py
    quick fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 
 ##session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME',col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
