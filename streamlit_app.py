ad1c03016c69c6a84e1e432119760ba1e68d90c0
AnniaLevko <57740640+AnniaLevko@users.noreply.github.com>
2024-04-15 09:41:22 -0400

    Update streamlit_app.py
    add api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -43,3 +44,7 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your smoothie is ordered, ' + name_on_order + '!')
+
+
+fruityvice_response = requests.get(https://fruityvice.com/api/fruit/watermelon)
+st.text(fruityvice_response)
